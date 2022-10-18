ed36472103e6e20ca70a126edf4df16e4a31717f
Jaqueline Malinoski <95326101+jaquemalinoski@users.noreply.github.com>
2022-10-18 15:50:27 -0300

    feat: creation input component

---
+++ client/src/components/input.tsx
@@ -0,0 +1,16 @@
+interface InputProps {
+  label: string
+  placeholder?: string
+  name: string
+  register: any
+  isErrored?: boolean
+}
+
+export const Input = ({placeholder, label, name, register}: InputProps) => {
+  return (
+    <label id='label'>
+      {label}
+      <input {...register(name)} className="input" placeholder={placeholder && placeholder}/>
+    </label>
+  )
+}
