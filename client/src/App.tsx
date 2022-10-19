32f29bef2de4595a95991ae5948839f6ced75fbd
Jaqueline Malinoski <95326101+jaquemalinoski@users.noreply.github.com>
2022-10-18 21:44:00 -0300

    feat: structure image modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import ModalCreateEditAd from "./components/modal create-edit ad";
 import ModalEditAddress from "./components/modal edit address";
+import ModalImage from "./components/modal image";
 import ModalSuccess from "./components/modal succes"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <ModalSuccess/>
         <ModalCreateEditAd/>
         <ModalEditAddress/>
+        <ModalImage/>
     </div>
   );
 }
